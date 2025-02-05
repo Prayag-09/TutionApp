3cfa0babb11e35b506385d11ae587ab133d72582
T Prayag <prayagtushar2016@gmail.com>
2025-02-05 12:23:19 +0530

    Changes

--- server/src/routes/protectedRoutes.ts
+++ server/src/routes/protectedRoutes.ts
@@ -1,16 +1,16 @@
-import express from 'express';
-import authenticate from '../middleware/auth';
-import { authorize } from '../middleware/authorize';
+// import express from 'express';
+// import authenticate from '../middleware/auth';
+// import { authorize } from '../middleware/authorize';
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get(
-	'/protected',
-	authenticate,
-	authorize(['Teacher', 'Principal']),
-	(req, res) => {
-		res.json({ message: 'You have accessed a protected route.' });
-	}
-);
+// router.get(
+// 	'/protected',
+// 	authenticate,
+// 	authorize(['Teacher', 'Principal']),
+// 	(req, res) => {
+// 		res.json({ message: 'You have accessed a protected route.' });
+// 	}
+// );
 
-export default router;
+// export default router;
